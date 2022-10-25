a1cf4979126644dbd4bc208e9fcd3538ef2e1b37
werick souza <werickxsilva@gmail.com>
2022-10-24 22:15:59 -0300

    feat: implementanto funcao duck
    reutilizando todo codigo da funcao run em duck
    atualizando update

--- dino_runner/components/dinosauro.py
+++ dino_runner/components/dinosauro.py
@@ -7,7 +7,6 @@ X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
 
-
 class Dinosauro:
     def __init__(self):
         self.image = RUNNING[0]
@@ -18,6 +17,7 @@ class Dinosauro:
         self.dino_run = True
         self.dino_jump = False
         self.jump_vel = JUMP_VEL
+        self.dino_duck = False
 
     # Metodo de atulizacao das acoes no jogo
     def update(self, user_input):
@@ -25,13 +25,22 @@ class Dinosauro:
             self.run()
         elif self.dino_jump:
             self.jump()
-
+        elif self.dino_duck:
+            self.duck()
+        
         if user_input[pygame.K_UP] and not self.dino_jump:
-            self.dino_jump = True
+            self.dino_duck = False
             self.dino_run = False
-        elif not self.dino_jump:
-            self.dino_jump = False 
+            self.dino_jump = True 
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
+        elif not (self.dino_jump or user_input[pygame.K_DOWN]):
+            self.dino_duck = False
             self.dino_run = True
+            self.dino_jump = False 
+
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -57,6 +66,11 @@ class Dinosauro:
             self.dino_jump = False
             self.jump_vel = JUMP_VEL
 
-#implementar metodo ate hoje 00
 
-        
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS + 59 # + pro dino nao ficar voando
+        # self.dino_rect.y = Y_POS * 2  # dino teleporta
+        self.step_index += 1
